29a1236f22eab51d21f15d2dfeaffa2383e9feb8
hachidakotomo <bahrulhuda88xtlas@gmail.com>
2018-07-26 19:30:27 +0700

    OK

---
+++ coba.sh
@@ -0,0 +1,58 @@
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+#Pm Gua Bangsat
+clear
+cd module
+python2 gua.py
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $blue
+cat asu.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : Mr CrOopPer"
+echo "\033[37;1mFeat: all member icp"
+echo "\033[35;1mversion Tools: 1"
+echo "\033[35;1mTeam :INDONESIA CYBER PROJECT"
+echo "\033[33;1mKontak Me : 6285334820020"
+echo "\033[31;1mauahgelap:"
+echo "\033[92miNDONESIA cYBER pROJECT" 
+echo "\033[36;1mAND ALL TEAM"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================]>"
+echo $green "1.> Spam Sms Tokopedia"
+echo "[===============================================]>"
+echo $purple "2.> Spam Sms Telkomnyet"
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[+]===============================================[+]"
+echo "\033[32;1m"
+read -p "root@T00Ls-MrCrOopPer~#" cuy
+
+if [ $cuy = 1 ] || [ $cuy = 1 ]
+then
+clear
+figlet "Mr CrOopPer" | lolcat
+php tp.php
+fi
+
+if
+[ $cuy = 2 ] || [ $cuy = 2 ]
+then
+clear
+toilet "Mr CrOopPer"
+php t.php
+fi
